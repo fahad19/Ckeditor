4b80208c22052b1a780de5cd60827863618537d2
Fahad Ibnay Heylaal <fahad19@gmail.com>
2013-03-02 22:15:46 +0600

    proper way of setting image URL in Ckeditor dialog.

--- webroot/js/wysiwyg.js
+++ webroot/js/wysiwyg.js
@@ -1,4 +1,16 @@
 Croogo.Wysiwyg.choose = function(url, title, description) {
-	window.top.opener.$('#cke_69_textInput').val(Croogo.Wysiwyg.uploadsPath + url);
-	window.top.close();
+	var params = window.location.href.split('?')[1].split('&');
+	var paramsObj = {};
+	for (var i in params) {
+		var param = params[i];
+		var paramE = param.split('=');
+		var k = paramE[0];
+		var v = paramE[1];
+		paramsObj[k] = v;
+	}
+
+	if (typeof paramsObj['CKEditorFuncNum'] != 'undefined') {
+		window.top.opener.CKEDITOR.tools.callFunction(paramsObj['CKEditorFuncNum'], Croogo.Wysiwyg.uploadsPath + url);
+		window.top.close();
+	}
 }
